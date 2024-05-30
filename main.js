f3e0c08350a62cb276d1b551aacb9390a4b1f0d2
Christine Peña <christine.penap@gmail.com>
2024-05-30 17:46:43 -0500

    add code

--- main.js
+++ main.js
@@ -1,24 +1,36 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+fetch("data.json")
+  .then((response) =>
+  {
+    if (!response.ok)
+    {
+      throw new Error(`HTTP error, status = ${response.status}`);
+    }
+    return response.json();
+  })
+  .then((data) =>
+  {
+    const frame = document.getElementById("loop");
+    data.map((element) =>
+    {
+      console.log(element);
+      let newDiv = document.createElement("div");
+      newDiv.id = element.id;
+      newDiv.className = "space";
+      newDiv.innerHTML = `
+            <div class="summary__item" data-color=${element.color}>
+            <div class="flex-group">
+            <img src=${element.icon} alt=""/>
+            <h4>${element.category}</h4>
+            </div>
+            <p class="summary__score">${element.score}
+            <span class="summary__light-text"> / 100</span>
+            </p></div>`
+        ;
+      frame.appendChild(newDiv);
+    });
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
 
-setupCounter(document.querySelector('#counter'))
+  }).catch((error) => console.error("Error loading JSON file", error));
+
+
