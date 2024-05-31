4e4513cc2f241bbd94225bf2b037af6e9866342c
Christine Peña <christine.penap@gmail.com>
2024-05-30 21:21:36 -0500

    add code17

--- main.js
+++ main.js
@@ -10,9 +10,27 @@ fetch('data.json')
   }
 
   )
-  .then(data => console.log("aqui los datos")) // <-- Aqui si esta data
+  .then((data) => // <-- Aqui si esta data
+  {
+    const frame = document.getElementById("loop");
+    data.map((element) =>
+    {
+      console.log(element);
+      let newDiv = document.createElement("div");
+      newDiv.id = element.id;
+      newDiv.className = "space";
+      newDiv.innerHTML = `
+        <div class="summary__item" data-color=${element.color}>
+        <div class="flex-group">
+        <img src=${element.icon} alt=""/>
+        <h4>${element.category}</h4>
+        </div>
+        <p class="summary__score">${element.score}
+        <span class="summary__light-text"> / 100</span>
+        </p></div>`
+        ;
+      frame.appendChild(newDiv);
+    });
 
 
-
-
-  .catch(error => console.log("Hay un error que no está entre 200 y 50x"));
+  }).catch(error => console.log("Hay un error que no está entre 200 y 50x"));
