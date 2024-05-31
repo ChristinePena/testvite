4d83c3eb069f4a2c16b91077390594ed506f83ba
Christine Peña <christine.penap@gmail.com>
2024-05-30 20:52:13 -0500

    add code14

--- main.js
+++ main.js
@@ -1,24 +1,3 @@
-fetch('data.json')
-  .then(function (response)
-  {
-    if (response.ok)
-    {
-      console.log("OK");
-      response.blob().then(function (miBlob)
-      {
-        var objectURL = URL.createObjectURL(miBlob);
-        miImagen.src = objectURL;
-      });
-    } else
-    {
-      console.log("Respuesta de red OK pero respuesta HTTP no OK");
-    }
-  })
-  .catch(function (error)
-  {
-    console.log("Hubo un problema con la petición Fetch:" + error.message);
-  });
-
 fetch('https://jsonplaceholder.typicode.com/todos/1')
   .then(response =>
   {
